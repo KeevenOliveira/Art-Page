8b91533d07b1237ecee04b564e050f48d21cdeb0
Keeven Oliveira <66172398+KeevenOliveira@users.noreply.github.com>
2021-09-26 21:37:46 -0300

    Presentation Created

---
+++ src/components/Presentation/styles.js
@@ -0,0 +1,84 @@
+import styled, { keyframes } from 'styled-components';
+
+
+const animation = keyframes`
+    0%{
+        stroke-width: 0;
+        stroke-dasharray: 1 100;
+        fill: transparent;
+    }
+    25%{
+        stroke-width: 3;
+        stroke-dasharray: 100 0;
+        fill: transparent;
+    }
+    100%{
+        stroke-width: 5;
+        fill: #2000AC;
+        stroke: #FAE22F;
+    }
+`;
+
+const animationContainer = keyframes`
+    /* from { width: 100% }
+    to { opacity: 0 } */
+    0% {
+        width: 0%
+    }
+    100% {
+        width: 100%;
+        /* display: none; */
+        visibility: hidden
+    }
+`;
+
+export const Container = styled.div`
+    position: absolute;
+    width: 100%;
+    top: 0;
+    display: flex;
+    justify-content: center;
+    height: 100vh;
+    overflow: hidden;
+    background-color: #2000AC;
+    z-index: 999;
+
+    animation: ${animationContainer} 3s linear alternate;
+`;
+
+export const Logo = styled.svg`
+    max-width: 200px;
+
+`;
+
+export const Path = styled.path`
+    fill: transparent;
+    animation: ${animation} 3s ease linear alternate;
+
+`;
+
+
+/**
+ * .logo-art{
+    fill: transparent;
+    animation: logo-art 4s ease infinite alternate;
+}}
+@   keyframes logo-art {
+    0%{
+        stroke-width: 0;
+        stroke-dasharray: 1 100;
+        fill: transparent;
+    }
+    25%{
+        stroke-width: 3;
+        stroke-dasharray: 100 0;
+        fill: transparent;
+    }
+    100%{
+        stroke-width: 5;
+        fill: #2000AC;
+        stroke: #FAE22F;
+    }
+}   
+ * 
+ */
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,10 +2,12 @@
 import Main from '../components/Main';
 import Footer from '../components/Footer';
 import Menu from '../components/Menu';
+// import Presentation from '../components/Presentation';
 
 const Dashboard = () =>{
   return (
     <>
+      {/* <Presentation/> */}
       <Main/>
       <Menu/>
       <Footer/>
